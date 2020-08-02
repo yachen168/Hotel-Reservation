031f99455a073cdb6848772f52bd12751a53b62b
yachen <io260188@gmail.com>
2020-08-02 11:02:04 +0800

    feat: initial home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
-	<div>
-		Home
-	</div>
+  <div class="home">
+    <header>
+      <Carousel :allRooms="allRooms"></Carousel>
+      <HeaderContent></HeaderContent>
+    </header>
+    <RoomCards :allRooms="allRooms"></RoomCards>
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+import HeaderContent from "@/components/HeaderContent";
+import Carousel from "@/components/Carousel";
+import RoomCards from "@/components/RoomCards";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  components: {
+    HeaderContent,
+    Carousel,
+    RoomCards
+  },
+  methods: {
+    ...mapActions({
+      getAllRooms: "getAllRooms"
+    })
+  },
+  created() {
+    this.getAllRooms();
+  },
+  computed: {
+    ...mapState({
+      allRooms: state => state.state.allRooms
+    })
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+header {
+  position: relative;
+}
 </style>
