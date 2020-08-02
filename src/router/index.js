ddc4a00baa9eeadcbc6e4db15d1f087528da6d7e
yachen <io260188@gmail.com>
2020-08-02 10:54:10 +0800

    feat: bind dynamic route to room cards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import RoomInfo from "../views/RoomInfo.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/room/:index",
+    name: "RoomInfo",
+    component: RoomInfo,
+    props: true
   }
 ];
 
