d4c8f24a61c61d2dabe8e3d71ad5887d8863441c
yachen <io260188@gmail.com>
2020-08-09 15:25:11 +0800

    feat: axios config

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const token = "ow7i3w9jLlmKW42w2FzKPVgSdHfI8G4dg20IZ1tzdGrMpfzyPAhQhIGAEJcr";
+
+const Axios = axios.create({
+  baseURL: `https://challenge.thef2e.com/api/thef2e2019/stage6/`,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${token}`
+  }
+});
+
+export default Axios;
