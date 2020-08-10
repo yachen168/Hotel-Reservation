c8572d9939346372e4d7194f5ef5bf7742feddef
yachen <io260188@gmail.com>
2020-08-11 00:06:29 +0800

    feat: scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -5,24 +5,26 @@ import RoomInfo from "../views/RoomInfo.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/room/:id",
-    name: "RoomInfo",
-    component: RoomInfo,
-    props: true
-  }
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/room/:id",
+        name: "RoomInfo",
+        component: RoomInfo,
+        props: true
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+    scrollBehavior() {
+        return { x: 0, y: 0 };
+    }
 });
 
 export default router;
