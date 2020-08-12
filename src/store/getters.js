18446453698ebcbc961d7ac1e55740d1c971e046
yachen <io260188@gmail.com>
2020-08-13 03:11:35 +0800

    feat: get the dates which have been booked

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,5 +4,10 @@ export default {
   },
   roomInfo({ state }) {
     return state.room[0];
+  },
+  datesHaveBeenBooked({ state }) {
+    return state.booking.map(value => {
+      return new Date(value.date);
+    });
   }
 };
