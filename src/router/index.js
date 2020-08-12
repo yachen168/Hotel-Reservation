0887bcbba2682f4b589f0f3417e58bb42789482a
yachen <io260188@gmail.com>
2020-08-13 03:35:49 +0800

    refactor: lazy loading routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import RoomInfo from "../views/RoomInfo.vue";
 
 Vue.use(VueRouter);
 
-const routes = [{
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/room/:id",
-        name: "RoomInfo",
-        component: RoomInfo,
-        props: true
-    }
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home")
+  },
+  {
+    path: "/room/:id",
+    name: "RoomInfo",
+    component: () => import("@/views/RoomInfo")
+  }
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes,
-    scrollBehavior() {
-        return { x: 0, y: 0 };
-    }
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  }
 });
 
 export default router;
