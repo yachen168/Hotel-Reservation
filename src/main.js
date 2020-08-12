f66844032db9296071ac1fea9fd690a623a25d5a
yachen <io260188@gmail.com>
2020-08-12 11:11:55 +0800

    import BootstrapVue and FontAwesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { BootstrapVue } from "bootstrap-vue";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+
+library.add(fas, far, fab);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
