87af8591143918a1ea76eab497005c6ebafe2a24
yachen <io260188@gmail.com>
2020-08-26 22:57:31 +0800

    refactor: grid

--- src/components/RoomInfo/Amenities.vue
+++ src/components/RoomInfo/Amenities.vue
@@ -1,81 +1,72 @@
 <template>
-  <b-row class="amenities">
-    <b-col cols="12" sm="4" class="amenities-column px-3">
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Wi-Fi'] }"
-      >
-        <font-awesome-icon icon="wifi" />Wi-Fi
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Breakfast'] }"
-      >
-        <font-awesome-icon icon="utensils" />早餐
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Mini-Bar'] }"
-      >
-        <font-awesome-icon icon="cocktail" />Bini Bar
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Room-Service'] }"
-      >
-        <font-awesome-icon icon="concierge-bell" />Room Service
-      </h4>
-    </b-col>
-    <b-col cols="12" sm="4" class="amenities-column">
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Television'] }"
-      >
-        <font-awesome-icon icon="phone" />電話
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Air-Conditioner'] }"
-      >
-        <font-awesome-icon icon="wind" />空調
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Refrigerator'] }"
-      >
-        <font-awesome-icon icon="temperature-low" />冰箱
-      </h4>
-      <h4 class="amenities-item" :class="{ serve: roomInfo.amenities['Sofa'] }">
-        <font-awesome-icon icon="couch" />沙發
-      </h4>
-    </b-col>
-    <b-col cols="12" sm="4" class="amenities-column">
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Great-View'] }"
-      >
-        <font-awesome-icon icon="mountain" />漂亮的視野
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Smoke-Free'] }"
-      >
-        <font-awesome-icon icon="smoking" />禁止吸煙
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Child-Friendly'] }"
-      >
-        <font-awesome-icon icon="baby" />適合兒童
-      </h4>
-      <h4
-        class="amenities-item"
-        :class="{ serve: roomInfo.amenities['Pet-Friendly'] }"
-      >
-        <font-awesome-icon icon="dog" />寵物攜帶
-      </h4>
-    </b-col>
-  </b-row>
+  <div class="amenities">
+    <h4 class="amenities-item" :class="{ serve: roomInfo.amenities['Wi-Fi'] }">
+      <font-awesome-icon icon="wifi" />Wi-Fi
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Breakfast'] }"
+    >
+      <font-awesome-icon icon="utensils" />早餐
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Mini-Bar'] }"
+    >
+      <font-awesome-icon icon="cocktail" />Bini Bar
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Room-Service'] }"
+    >
+      <font-awesome-icon icon="concierge-bell" />Room Service
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Television'] }"
+    >
+      <font-awesome-icon icon="phone" />電話
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Air-Conditioner'] }"
+    >
+      <font-awesome-icon icon="wind" />空調
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Refrigerator'] }"
+    >
+      <font-awesome-icon icon="temperature-low" />冰箱
+    </h4>
+    <h4 class="amenities-item" :class="{ serve: roomInfo.amenities['Sofa'] }">
+      <font-awesome-icon icon="couch" />沙發
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Great-View'] }"
+    >
+      <font-awesome-icon icon="mountain" />漂亮的視野
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Smoke-Free'] }"
+    >
+      <font-awesome-icon icon="smoking" />禁止吸煙
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Child-Friendly'] }"
+    >
+      <font-awesome-icon icon="baby" />適合兒童
+    </h4>
+    <h4
+      class="amenities-item"
+      :class="{ serve: roomInfo.amenities['Pet-Friendly'] }"
+    >
+      <font-awesome-icon icon="dog" />寵物攜帶
+    </h4>
+  </div>
 </template>
 
 <script>
@@ -91,12 +82,14 @@ export default {
 
 <style lang="scss" scoped>
 .amenities {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   padding: 15px;
   margin-top: 40px;
   background-color: $gray-300;
+  @media (min-width:576px) {
+      grid-template-columns: 1fr 1fr 1fr;
+  }
   &-item {
     display: flex;
     align-items: center;
