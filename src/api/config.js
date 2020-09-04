b0f41e84c079eb6898d7895af646a98a9f93f79c
yachen <io260188@gmail.com>
2020-09-05 02:17:50 +0800

    fix: request timeout

--- src/api/config.js
+++ src/api/config.js
@@ -8,7 +8,8 @@ const Axios = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
     Authorization: `Bearer ${token}`
-  }
+  },
+  timeout: 15000
 });
 
 export default Axios;
