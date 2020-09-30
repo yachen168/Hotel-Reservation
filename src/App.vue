5344d57326476ddcb533edf9e240b383e5e2e7d1
yachen <io260188@gmail.com>
2020-09-30 23:35:11 +0800

    refactor: remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,6 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  position: relative;
 }
 </style>
