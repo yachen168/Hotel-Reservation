fb08fe7cc56cc5f505d0903e311374a4f4a55308
yachen <io260188@gmail.com>
2020-10-13 12:19:56 +0800

    feat: networkIssue page

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="network-issue">
+    <h1>Oh! 網路似乎存在一些問題</h1>
+    <h2>請再試一次 :)</h2>
+    <router-link :to="{ name: 'Home' }">點擊返回首頁</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.network-issue {
+  text-align: center;
+}
+</style>
